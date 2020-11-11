b3421c73b9afab0dab8d8d390d0df228c5e09ecf
Edwin de Jonge <edwindjonge@gmail.com>
2020-11-12 00:20:20 +0100

    tweaking

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(validatedb)
+
+test_check("validatedb")
---
+++ tests/testthat/test-confront_wide.R
@@ -0,0 +1,24 @@
+describe("rule_works_on_db", {
+  it ("works with numeric checks",{
+    rules <- validator(x > 1, y < x, x == 0)
+    con <- dbplyr::src_memdb()
+    
+    d <- data.frame(x = 1, y = 2)
+    tbl_d <- dplyr::copy_to(con, d, overwrite=TRUE)
+    
+    res <- rule_works_on_db(tbl_d, rules)
+    expect_equal(res, c(TRUE, TRUE, TRUE))
+  })
+  
+  it ("fails on function not on db",{
+    f <- function(x) x
+    rules <- validator(x > 1, f(x) > 0)
+    con <- dbplyr::src_memdb()
+    
+    d <- data.frame(x = 1, y = 2)
+    tbl_d <- dplyr::copy_to(con, d, overwrite=TRUE)
+    
+    res <- rule_works_on_db(tbl_d, rules)
+    expect_equal(res, c(TRUE, FALSE))
+  })
+})
