a9a0b0cc2fcfa098df211ddf99c83bd66a294cc6
Edwin de Jonge <edwindjonge@gmail.com>
2021-12-01 15:18:51 +0100

    tweaking

--- R/unsparse.R
+++ R/unsparse.R
@@ -1,3 +1,4 @@
+#' @importFrom dplyr na_if
 unsparse <- function(cf){
  key <- lapply(cf$key, as.symbol)
 
@@ -13,13 +14,17 @@ unsparse <- function(cf){
  rnms <- names(cf$exprs)
  c_cols <- 
    lapply(rnms, function(n){
-    bquote(switch(rule, ..(elist(n = dplyr::coalesce(!fail, -1L))), TRUE), splice = TRUE)
+    bquote(switch( rule
+                 , ..(elist(n = dplyr::coalesce(1L - fail, -1L))) #invalid = 0, or NA = -1
+                 , 1L  # valid = 1
+                 ), splice = TRUE)
  })
  names(c_cols) <- rnms
  
  passes <- dplyr::transmute(fails, !!!key, !!!c_cols)
  passes <- dplyr::group_by(passes, !!!key)
  # and summarise to have the same rows are the original table
- passes <- dplyr::summarise(passes, dplyr::across(!!rnms, ~ dplyr::na_if(min(.x, na.rm=TRUE), -1L), na.rm=TRUE))
+ passes <- dplyr::summarise(passes, dplyr::across(!!rnms, min, na.rm=TRUE))
+ passes <- dplyr::mutate(passes, dplyr::across(!!rnms, na_if, -1L))
  passes
 }
