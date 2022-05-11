f0fdc29dc052a8cfcad7434cd48dd480c40e918f
Edwin de Jonge <edwindjonge@gmail.com>
2022-05-11 11:45:18 +0200

    added violating function

---
+++ R/violating.R
@@ -0,0 +1,17 @@
+violating.tbl_sql <- function(x, y, include_missing=FALSE, key  = NULL, ...){
+  if (inherits(y, "validator")){
+    y <- confront(x, y, key = key, ...)
+  }
+  qry <- y$query
+  if (!isTRUE(include_missing)){
+    qry <- dplyr::filter(qry, fail == 1)
+  }
+  keys <- lapply(key, as.symbol)
+  qry <- dplyr::distinct(qry, !!!keys)
+  
+  dplyr::semi_join(y$tbl, qry,by = key)
+}
+
+satisfying.tbl_sql <- function(x,y, include_missing = FALSE, key = NULL, ...){
+  
+}
